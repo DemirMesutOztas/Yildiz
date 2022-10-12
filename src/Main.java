39a4bdd0a846e7be16acfcbc60a3514c49b69072
Gogle Öztaş <demiroztas06@hotmail.com>
2022-10-12 15:40:12 +0300

    son

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <n ; i++)
+        {
+            for (int j = 0; j < (n - i); j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int i = n - 1; i > 0; i--)
+        {
+
+            for (int k = 0; k <= (n - i); k++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
